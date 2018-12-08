a14cbe9a89e0198bc4e7304dd37a9a8ac0df41de
Deepcooler <Deepcooler@10.77.16.85>
2018-12-09 00:56:22 +0300

    delta

--- src/main/java/com/simple/server/config/AppConfig.java
+++ src/main/java/com/simple/server/config/AppConfig.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.LinkedBlockingQueue;
 
+
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 import org.hibernate.SessionFactory;
